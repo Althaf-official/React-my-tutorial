3fd4d4843999cfec8b3db19d73269846ae067fd3
Althaf Sharafudheen <althaf.sharafudheen@outlook.com>
2022-05-24 18:49:17 +0400

    react should not refresh all  page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import About from "./Container/About"
 import Profile from "./Container/Profile"
 import { useState } from 'react'
-import {BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
 function App() {
   const [state, setState] = useState('')
 
   return (
     <div className='App'>
-      <button onClick={() => setState('about')} >About</button>
-      <button onClick={() => setState('profile')}>Profile</button>
-
       <Router>
         <Routes>
           <Route path="/about" element={<About />}/>
