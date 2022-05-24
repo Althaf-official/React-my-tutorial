eaf0beb1dc88f03054414aff21349ee2e6508d34
Althaf Sharafudheen <althaf.sharafudheen@outlook.com>
2022-05-24 14:59:20 +0400

    created two if conditions

--- src/App.js
+++ src/App.js
@@ -5,14 +5,18 @@ import {useState} from 'react'
 function App() {
   const[state,setState] =useState('')
   let component
+  if (state==='about'){
+    component=<About></About>
+  }
+  if(state==='profile'){
+    component=<Profile></Profile>
+
+  }
 
   return (
     <div className='App'>
     <button onClick={()=>setState('about')} >About</button>
     <button onClick={()=>setState('profile')}>Profile</button>
-
-     <About></About>
-     <Profile></Profile>
     </div>
   )
 }
