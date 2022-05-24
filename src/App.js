026caa03da6fe8e8e04e60d405cf137df3ca6b0f
Althaf Sharafudheen <althaf.sharafudheen@outlook.com>
2022-05-24 14:18:32 +0400

    create two buttons . i have to make like when we click the button then only show the pages

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ import Profile from "./Container/Profile"
 function App() {
   return (
     <div className='App'>
+
+    <button>About</button>
+    <button>Profile</button>
+
      <About></About>
      <Profile></Profile>
     </div>
