f763baa3e0121fb5fa66a57c8835dee6d8ed8280
Althaf Sharafudheen <althaf.sharafudheen@outlook.com>
2022-05-25 21:32:36 +0400

    button - succesfully navigate the import the navigate button and when we click the button it will navigate to another page

--- src/App.js
+++ src/App.js
@@ -5,12 +5,12 @@ import { Routes, Route, Link,Router1, useNavigate } from "react-router-dom";
 
 function App() {
   const [state, setState] = useState('')
-  const history =useNavigate()
+  const navigate =useNavigate()
 
   return (
     <div className='App'>
-        <button onClick={()=>history.push('/about')}>About me</button>
-        <button onClick={()=>history.push('/profile')}>profile</button>
+        <button onClick={()=>navigate('/about')}>About me</button>
+        <button onClick={()=>navigate('/profile')}>profile</button>
      
         <Routes>
           <Route path="/about" element={<About />} />
