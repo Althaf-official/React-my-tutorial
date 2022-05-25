0d6547df5b0efa6c21338d364fa10aac16974d7f
Althaf Sharafudheen <althaf.sharafudheen@outlook.com>
2022-05-25 21:26:18 +0400

    button succesfully created

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import About from "./Container/About"
 import Profile from "./Container/Profile"
 import { useState } from 'react'
-import { Routes, Route, Link,useHistory } from "react-router-dom";
+import { Routes, Route, Link,Router1, useNavigate } from "react-router-dom";
 
 function App() {
   const [state, setState] = useState('')
-  const history =useHistory()
+  const history =useNavigate()
 
   return (
     <div className='App'>
-        <Link to='/about'>About Page</Link>
-        <Link to='/profile'>Profile page</Link>
+        <button onClick={()=>history.push('/about')}>About me</button>
+        <button onClick={()=>history.push('/profile')}>profile</button>
+     
         <Routes>
           <Route path="/about" element={<About />} />
           <Route path="/profile" element={<Profile/>} />
         </Routes>
+   
       
  
 
