5e6f516ad3651877db342984ded380fc696fcacd
Althaf Sharafudheen <althaf.sharafudheen@outlook.com>
2022-05-25 21:01:16 +0400

    Button  in react setup

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import About from "./Container/About"
 import Profile from "./Container/Profile"
 import { useState } from 'react'
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
   const [state, setState] = useState('')
 
   return (
     <div className='App'>
-      <Router>
+      
 
         
         <Link to='/about'>About Page</Link>
@@ -17,7 +17,7 @@ function App() {
           <Route path="/about" element={<About />} />
           <Route path="/profile" element={<Profile/>} />
         </Routes>
-      </Router>
+      
  
 
     </div>
