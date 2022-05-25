04c420fd33de76a1edcb967c3069079e51c26310
Althaf Sharafudheen <althaf.sharafudheen@outlook.com>
2022-05-25 21:05:52 +0400

    button - import useHistory

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import About from "./Container/About"
 import Profile from "./Container/Profile"
 import { useState } from 'react'
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, Link,useHistory } from "react-router-dom";
 
 function App() {
   const [state, setState] = useState('')
+  const history =useHistory()
 
   return (
     <div className='App'>
-      
-
-        
         <Link to='/about'>About Page</Link>
         <Link to='/profile'>Profile page</Link>
         <Routes>
