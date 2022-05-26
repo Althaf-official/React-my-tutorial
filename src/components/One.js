8d2c08b305a2754dfd9ad819cb259a490d0f8338
Althaf Sharafudheen <althaf.sharafudheen@outlook.com>
2022-05-26 05:42:56 +0400

    insert data in the two files. now we need take this data inside of the profile

---
+++ src/components/One.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function One() {
+  return (
+    <div>
+      <h1>Layer one</h1>
+    </div>
+  )
+}
+
+export default One
---
+++ src/components/Two.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Two() {
+  return (
+    <div>
+      <h1>leyer two</h1>
+    </div>
+  )
+}
+
+export default Two
