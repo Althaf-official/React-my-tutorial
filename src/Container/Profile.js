5f5e8f69a189e8ca2c41ed38068c03a218374b1a
Althaf Sharafudheen <althaf.sharafudheen@outlook.com>
2022-05-26 05:52:23 +0400

    import two.js in one.js

--- src/Container/Profile.js
+++ src/Container/Profile.js
@@ -1,10 +1,12 @@
 import React from 'react'
+import One from '../components/One'
 import "./profile.css"
 
 function Profile() {
   return (
     <div className='profile'>
       <h1>Profile</h1>
+      <One></One>
     </div>
   )
 }
--- src/components/One.js
+++ src/components/One.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import Two from './Two'
 
 function One() {
   return (
     <div>
       <h1>Layer one</h1>
+      <Two></Two>
     </div>
   )
 }
